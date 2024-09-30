ad49876e1b87e62a8e874cb8f52432a9437bc510
shushant kumar <shushantkumar0625@gmail.com>
2024-09-30 15:21:34 +0530

    adding gsap in home page

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 gsap.registerPlugin(ScrollTrigger)
 
+var typed = new Typed('#text',{
+    strings: ['Frontend Developer', 'Software Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+    // blink: true,
+    // blinkSpeed: 500,
+    blink: false, // Set blink to false to disable the blinking effect
+    showCursor: false 
+}); 
+
 
 // Create a GSAP timeline
 var tl = gsap.timeline();
@@ -27,12 +39,14 @@ tl.from(".navbar a", {
     },
     ease: "power3.out" // Use a smoother easing function
 });
-// tl.from(".home", {
-//     y: -1500,
-//     duration: 1.2, // Slightly increase the duration to give a smoother effect
-//     opacity: 0,
-//     ease: "power3.out" // Use a smoother easing function
-// });
+tl.from(".star",{
+    rotation: 360,
+    duration: 2, // adjust the duration to your liking
+    ease: "power2.inOut", // adjust the easing to your liking
+    repeat: -1, // repeat indefinitely
+    delay:2
+})
+
 
  gsap.utils.toArray('.star').forEach(star=>{
     gsap.fromTo(".star",{
