90a1c4c2453deacb80129e54e89e53d591252a8e
shushant kumar <shushantkumar0625@gmail.com>
2024-10-01 20:36:27 +0530

    add pages

--- script.js
+++ script.js
@@ -46,8 +46,6 @@ tl.from(".star",{
     repeat: -1, // repeat indefinitely
     delay:2
 })
-
-
  gsap.utils.toArray('.star').forEach(star=>{
     gsap.fromTo(".star",{
         rotation:0,
@@ -62,6 +60,27 @@ tl.from(".star",{
         scrollTrigger:star
     })
  })
+ 
+
+ // Select all navigation links
+ document.querySelectorAll('.navbar a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+  
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
+      }
+    });
+  });
+
+
+  
+  
 
 
 
